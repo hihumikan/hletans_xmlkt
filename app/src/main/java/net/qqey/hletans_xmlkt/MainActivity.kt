2314e41baa761fa44759f33afcd8fc69b41b8425
hihumikan <a@qqey.net>
2024-03-31 23:31:31 +0900

    viewの取得

--- app/src/main/java/net/qqey/hletans_xmlkt/MainActivity.kt
+++ app/src/main/java/net/qqey/hletans_xmlkt/MainActivity.kt
@@ -1,12 +1,16 @@
 package net.qqey.hletans_xmlkt
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,14 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val homeLocation:EditText = findViewById(R.id.home_location_form)
+        val currentLocation:EditText = findViewById(R.id.current_location_form)
+        val travelMode:EditText = findViewById(R.id.travel_mode_form)
+        val webhookURL:EditText = findViewById(R.id.webhook_url_form)
+        val endpointURL:EditText = findViewById(R.id.endpoint_url_form)
+
+        val getLocationButton: Button = findViewById(R.id.get_locate)
+        val sendButton: Button = findViewById(R.id.send_button)
+
     }
 }
